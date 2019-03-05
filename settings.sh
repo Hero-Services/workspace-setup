e634a6430b056f467cb1e38893acb9819546e03d
Taylor Tobin <19tt94@gmail.com>
2019-03-05 10:03:43 -0800

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -10,15 +10,16 @@ echo *.DS_Store >> ~/.gitignore
 
 # terminal settings | create bash_profile
 cat bash_profile.sh > ~/.bash_profile.sh
+# initialize bash_profile
 source ~/.bash_profile
 
 # create nvm directory
 mkdir ~/.nvm
 
 # tmux settings | create tmux.conf
-touch tmux.conf
-# copy over tmux.conf
 cat tmux.conf > ~/.tmux.conf
+# initialize .tmux.conf
+tmux source-file ~/.tmux.conf
 
 # vim settings | create .vimrc
 touch .vimrc
