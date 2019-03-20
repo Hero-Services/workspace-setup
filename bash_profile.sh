575535bf05da660698393777a791cd8772bad5a7
Taylor Tobin <19tt94@gmail.com>
2019-03-20 09:07:04 -0700

    Update bash_profile.sh

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,23 +1,20 @@
-# !/bin/bash
+#!/bin/bash
+
+if [ -r ~/.profile ]; then ~/.profile; fi
+case "$-" in *i*) if [ -r ~/.bashrc ]; then . ~/.bashrc; fi;; esac
 
 # General
 export PS1="⚓  [ \u ]:\W $ "
 export CLICOLOR=1
 export LSCOlORS=ExFxCxDxBxegedabagacad
-alias home='cd ~/Desktop'
-
-# Application Commands
-alias phone='open/Applications/Xvode.app/Contents/Developer/Applications/Simulator.app'
-alias server='python3 -m http.server'
-alias chrome='open -a "Google Chome" --args'
-alias atom='atom .'
-alias open='open .'
 
 # Load NVM
-export NVM_DIR="$HOME/.nvm"
-  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+export NVM_DIR="$HOME/.nvm"	export NVM_DIR=~/.nvm
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm	source $(brew --prefix nvm)/nvm.sh
   [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
 
 # Setting PATH for Python 3.6
-PATH="/Library/Frameworks/Python.framework/Versions/3.6/bin:${PATH}"
-export PATH
+export PATH="/Library/Frameworks/Python.framework/Versions/3.6/bin:${PATH}"
+
+# Setting PATH for VSCode
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
