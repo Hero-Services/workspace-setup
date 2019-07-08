5e15427d3ac50a548fe8c85e32d84573554666f3
Taylor Tobin <19tt94@gmail.com>
2019-07-08 16:58:15 -0700

    scaffolded first option

--- index.js
+++ index.js
@@ -17,32 +17,32 @@ program
           .prompt([
             {
               type: 'list',
-              name: 'theme',
+              name: 'selection',
               message: 'What do you want to do?',
               choices: [
-                'Order a pizza',
-                'Make a reservation',
-                new inquirer.Separator(),
-                'Ask for opening hours',
-                {
-                  name: 'Contact support',
-                  disabled: 'Unavailable at this time'
-                },
-                'Talk to the receptionist'
+                'Install Everything',
+                'Only Install DevTools'
               ]
-            },
-            {
-              type: 'list',
-              name: 'size',
-              message: 'What size do you need?',
-              choices: ['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro'],
-              filter: function(val) {
-                return val.toLowerCase();
-              }
             }
           ])
           .then(answers => {
             console.log(JSON.stringify(answers, null, '  '));
+
+            inquirer
+                .prompt([
+                    {
+                      type: 'list',
+                      name: 'choices',
+                      message: 'Select options to ignore (if any)',
+                      choices: ['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro'],
+                      filter: function(val) {
+                        return val.toLowerCase();
+                      }
+                    }
+                ])
+                .then(answers => {
+                    
+                })
           });
   })
   .parse(process.argv);
